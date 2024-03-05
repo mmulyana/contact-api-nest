c174e60103a3b093b23897eda8234763949f8ac3
mmulyana <mulyan.t20@gmail.com>
2024-03-06 00:00:00 +0700

    feat: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CommonModule } from './common/common.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
+  imports: [CommonModule, UserModule],
   controllers: [],
   providers: [],
 })
